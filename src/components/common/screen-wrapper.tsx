7501a1a8ec573813df5a899f40a3193ae5752438
anshIA09 <Anish.Pawaskar@interactiveavenues.com>
2025-11-13 16:24:10 +0530

    feat: create screen wrapper

---
+++ src/components/common/screen-wrapper.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const ScreenWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-screen flex items-center justify-center">{children}</div>
+  );
+};
